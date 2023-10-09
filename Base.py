c9ba7d984ecfd0242f35a5087bf8ba685e241a13
Regis Mallet <regismallet1@gmail.com>
2023-10-09 06:45:54 +0200

    Commit ?

--- Base.py
+++ Base.py
@@ -1,2 +1,14 @@
 # C'est parti
 
+# Rajout de ce commentaire
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bonjour, bienvenue sur mon application web !"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
